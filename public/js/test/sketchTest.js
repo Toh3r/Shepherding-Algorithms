64f73549be69de4993919a50c36e2b31a92ab5bc
Daniel Toher <dan.t@live.ie>
2019-05-16 13:24:25 +0100

    images broken on push

--- public/js/test/sketchTest.js
+++ public/js/test/sketchTest.js
@@ -6,10 +6,10 @@ function setup() {
 
   // Create Canvas
   var canvas = createCanvas(1000,600);
-  img = loadImage('../css/images/Field1B.jpg');
-  img2 = loadImage('../css/images/Field_1.jpg');
-  img3 = loadImage('../css/images/Field_2.jpg');
-  img4 = loadImage('../css/images/Field_3.jpg');
+  img = loadImage('./css/images/Field1B.jpg');
+  img2 = loadImage('./css/images/Field_1.jpg');
+  img3 = loadImage('./css/images/Field_2.jpg');
+  img4 = loadImage('./css/images/Field_3.jpg');
   canvas.parent('myCanvas'); // .parent allows item to manipulated on html page
 
   // Set framerate of canvas
